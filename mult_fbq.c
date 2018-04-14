9e0bfc5a372ab8d843324e2ee660082d05a17e41
N S Vishnu <37975368+NSVishnu@users.noreply.github.com>
2018-04-15 00:38:03 +0530

    Update mult_fbq.c

--- mult_fbq.c
+++ mult_fbq.c
@@ -1,3 +1,5 @@
+//multi level feedback queue
+
 #include<stdio.h>
 #include<string.h>
 #include<conio.h>
@@ -55,10 +57,95 @@ main()
 	}
 	for(i=0;i<n;i++)
 	{
-		printf("%s \n",Q[i].na);
+		//printf("%s \n",Q[i].na);
 		burst=burst+Q[i].bt;
 	}
 	int gant[burst];
 	
 	int start=Q[0].at;
 	printf("Queue 1 : ROUND ROBIN SCHEDULING OF TIME QUANTUM =8");
+	for(i=0;i<n;i++)
+	{
+		printf("\n\nProcess %s starts from %d ",Q[i].na,start);
+		if(Q[i].at<=start)
+		{
+			if(Q[i].bt==qt)
+			{
+				start=start+qt;
+				Q[i].rem=0;
+			/*for(j=i;j<n-1;j++)
+			{
+				Q[i].at=Q[i+1].at;
+				Q[i].bt=Q[i+1].bt;
+				Q[i].rem=Q[i+1].rem;
+				Q[i].pr=Q[i+1].pr;
+				strcpy(Q[i].na,Q[i+1].na);
+			}*/
+			}
+			if(Q[i].bt<qt)
+			{
+				start=start+Q[i].bt;
+				Q[i].rem=0;
+			}
+			if(Q[i].bt>qt)
+			{
+				start=start+qt;
+				Q[i].rem=Q[i].bt-qt;
+			}
+		}
+		printf(" and ends on %d",start);
+	}
+	
+	qt=16;
+	printf("\n\nQueue 2 : ROUND ROBIN SCHEDULING OF TIME QUANTUM =16");
+	for(i=0;i<n;i++)
+	{
+		if(Q[i].rem==0)
+		{
+			printf("\n\nProcess %s completed processing",Q[i].na);
+			continue;
+		}
+		
+		printf("\n\nProcess %s starts from %d ",Q[i].na,start);
+		if(Q[i].at<=start)
+		{
+			if(Q[i].rem==qt)
+			{
+				start=start+qt;
+				Q[i].rem1=0;
+			}
+			if(Q[i].rem<qt)
+			{
+				start=start+Q[i].rem;
+				Q[i].rem1=0;
+			}
+			if(Q[i].rem>qt)
+			{
+				start=start+qt;
+				Q[i].rem1=Q[i].rem-qt;
+			}
+			
+		}
+		
+		printf(" and ends on %d",start);
+	}
+	
+	printf("\n\nQueue 3 : FIRST COME FIRST SERVE ");
+	for(i=0;i<n;i++)
+	{
+		if(Q[i].rem1==0)
+		{
+			printf("\n\nProcess %s completed processing",Q[i].na);
+			continue;
+		}
+		
+		printf("\n\nProcess %s starts from %d ",Q[i].na,start);
+		if(Q[i].at<=start)
+		{
+			start=start+Q[i].rem1;
+		}
+		
+		printf(" and ends on %d",start);
+	}
+
+}
